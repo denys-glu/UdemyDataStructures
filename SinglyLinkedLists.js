6f7334f1de999474ef6699efd2cb1c659ee76df2
denys_glu <denis.glukhovskyy@gmail.com>
2021-04-05 15:51:56 -0700

    added printList method and pop method

--- SinglyLinkedLists.js
+++ SinglyLinkedLists.js
@@ -13,7 +13,7 @@ class SinglyLinkedList {
   }
 
   push(val) {
-    const newNode = new Node(val)
+    const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
@@ -23,6 +23,44 @@ class SinglyLinkedList {
     }
 
     this.length += 1;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined
+    } else {
+      let walker = this.head;
+      let runner = this.head.next;
+      while (runner) {
+        walker = walker.next;
+        runner = runner.next;
+      }
+      walker.next = null;
+      this.tail = walker;
+    }
+
+    this.length += 1;
+    return this;
+  }
+
+  printList() {
+    if (this.head == null) {
+      console.log("The list is empty.");
+      return this;
+    }
+    let str = "H: ";
+    let runner = this.head;
+    while (runner != null) {
+      if (runner.next === null) {
+        str += `T: ${runner.val}`
+      } else {
+        str += `${runner.val} -> `
+      }
+      runner = runner.next;
+
+    }
+    console.log(str);
     return this;
   }
 
