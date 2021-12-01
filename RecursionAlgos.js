55a8e2f6ef46ce96e2a1c2934000cbcfef7d7dd8
denys_glu <denis.glukhovskyy@gmail.com>
2021-11-30 17:59:05 -0800

    Added recursive function that applies callback during iteration

--- RecursionAlgos.js
+++ RecursionAlgos.js
@@ -101,3 +101,21 @@ function isPalindrome(str){
 // isPalindrome('tacocat') // true
 // isPalindrome('amanaplanacanalpanama') // true
 // isPalindrome('amanaplanacanalpandemonium') // false
+
+
+// Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
+
+function someRecursive(arr, callback){
+  // add whatever parameters you deem necessary - good luck!
+  if (arr.length === 0) return false;
+  if (callback(arr[0])) return true;
+  return someRecursive(arr.splice(1), callback);
+}
+
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val =&gt; val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val =&gt; val &gt; 10); // false
